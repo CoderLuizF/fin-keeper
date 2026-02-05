d7e4e59bb391a53470bbd5ebe25b9782cf4b086e
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-05 17:50:12 -0300

    Enable JSON body parsing middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const app = express();
-
 const PORT = 3001;
 
+app.use(express.json());
+
 app.get("/health", (req, res) => {
-    res.json({status: 'API is running'})
-})
+  res.json({ status: "API is running" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
