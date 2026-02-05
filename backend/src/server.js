4391893755cc039433044f60d4811fd1aa28434e
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-05 17:26:45 -0300

    Create basic Express HTTP server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,9 @@
-const express = require('express')
+const express = require("express");
+
+const app = express();
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
