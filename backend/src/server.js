0aa5b5bc4a96ff4356b41029470ea6c46180bc24
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-05 18:20:44 -0300

    Configure environment variables with dotenv

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const healthRoutes = require("./routes/healthRoutes");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
