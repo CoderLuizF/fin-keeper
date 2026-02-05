3d3289acdcbd7f3b9dab76d453e832a3e1381fe1
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-05 17:36:05 -0300

    Add health check API route

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3001;
 
+app.get("/health", (req, res) => {
+    res.json({status: 'API is running'})
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
