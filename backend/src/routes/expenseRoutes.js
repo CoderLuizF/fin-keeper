536773f59a9a6e800900cd69bc917581b55c30a4
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-09 18:45:25 -0300

    Apply validation middleware to expense creation

--- backend/src/routes/expenseRoutes.js
+++ backend/src/routes/expenseRoutes.js
@@ -6,11 +6,25 @@ const {
   updateExpense,
 } = require("../controller/expenseController");
 
+const { expenseValidation } = require("../validators/expenseValidator");
+const { validationResult } = require("express-validator");
+
 const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post("/expenses", protect, createExpense);
+router.post(
+  "/expenses",
+  protect,
+  expenseValidation,
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  },
+  createExpense,
+);
 router.get("/expenses", protect, getExpenses);
 router.delete("/expenses/:id", protect, deleteExpense);
 router.put("/expenses/:id", protect, updateExpense);
