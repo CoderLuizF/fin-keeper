010110b0ad2bf88271e0be84e9c4f24c9aa0e1c5
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-09 18:11:24 -0300

    Create expense summary controller

---
+++ backend/src/controller/summaryController.js
@@ -0,0 +1,30 @@
+const Expense = require("../models/Expense");
+
+const getSummary = async (req, res) => {
+  try {
+    const expense = await Expense.find({ user: req.user.id });
+
+    const total = expenses.reduce((acc, expense) => {
+      return acc + expense.amount;
+    }, 0);
+
+    const byCategory = {};
+
+    expenses.forEach((expense) => {
+      if (!byCategory[expense.category]) {
+        byCategory[expense.category] = 0;
+      }
+
+      byCategory[expense.category] += expense.amount;
+    });
+
+    res.json({
+      total,
+      byCategory,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { getSummary };
