cb120329f16f172924c47552f719a75acd25155f
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-10 09:34:18 -0300

    Add category model

---
+++ backend/src/models/Category.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const categorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Category", categorySchema);
