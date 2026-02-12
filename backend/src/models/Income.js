daadbaae4c83745457ea3a9330e96f57d8c53ebe
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-11 22:22:38 -0300

    feat: create income model

---
+++ backend/src/models/Income.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const incomeSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Income", incomeSchema);
