ebd533cae8e93e5f8724c7a3a204ac41220ab39a
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-16 08:41:29 -0300

    feat: create auth service with login, register and token handling

---
+++ frontend/fin-keeper/src/services/authService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3001/api/auth";
+
+export async function registerUser(userData) {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+}
+
+export async function loginUser(userData) {
+  const response = await axios.post(`${API_URL}/login`, userData);
+
+  if (response.data.token) {
+    localStorage.setItem("token", response.data.token);
+  }
+
+  return response.data;
+}
+
+export function logoutUser() {
+  localStorage.removeItem("token");
+}
