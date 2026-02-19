8b6a715c6f28093874524fdc7fbae0a495ad5962
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-19 20:57:43 -0300

    refactor: remove localStorage logic from auth service

--- frontend/fin-keeper/src/services/authService.js
+++ frontend/fin-keeper/src/services/authService.js
@@ -9,11 +9,6 @@ export async function registerUser(userData) {
 
 export async function loginUser(userData) {
   const response = await axios.post(`${API_URL}/login`, userData);
-
-  if (response.data.token) {
-    localStorage.setItem("token", response.data.token);
-  }
-
   return response.data;
 }
 
