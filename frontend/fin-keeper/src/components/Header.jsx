a00f5fcc0d96e705f03154f39d01b25fc88b7978
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-23 23:14:46 -0300

    feat: updated header

--- frontend/fin-keeper/src/components/Header.jsx
+++ frontend/fin-keeper/src/components/Header.jsx
@@ -1,10 +1,27 @@
-function Header() {
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+export default function Header() {
+  const { isAuthenticated, logout } = useAuth();
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    logout();
+    navigate("/login");
+  }
+
   return (
-    <header className="bg-white px-5 py-4 border-b border-gray-300 flex justify-between items-center">
-      <h1>Dashboard</h1>
-      <button>Logout</button>
+    <header className="bg-white border-b px-6 py-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold">FinKeeper</h1>
+
+      {isAuthenticated && (
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
+        >
+          Logout
+        </button>
+      )}
     </header>
   );
 }
-
-export default Header;
