192714effeafa4b9fc48337e6684a7822e2a4805
Luiz Felipe <zzzezizzz@gmail.com>
2026-02-23 23:28:38 -0300

    fix: wrap application with BrowserRouter to enable routing

--- frontend/fin-keeper/src/App.jsx
+++ frontend/fin-keeper/src/App.jsx
@@ -1,24 +1,29 @@
 import { Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+
 import ProtectedRoute from "./routes/ProtectedRoute";
 import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
     <Routes>
+      {/* Rotas públicas */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
+      {/* Rotas privadas */}
       <Route
-        path="/dashboard"
         element={
           <ProtectedRoute>
-            <Dashboard />
+            <MainLayout />
           </ProtectedRoute>
         }
-      />
+      >
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 }
